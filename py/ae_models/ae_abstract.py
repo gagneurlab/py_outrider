fixed loss calculation, inserted ae_bfgs_cov1 consideration, tested on hesse

py/ae_models/ae_abstract.py
@@ -134,10 +134,10 @@ class Ae_abstract(ABC):
             par_meas = np.zeros(shape=(X_true.shape[1], ))
 
         ### return np or tf
-        if tf.is_tensor(X_true_pred):
-            return tf.convert_to_tensor(par_meas, dtype=X_true_pred.dtype)
-        else:
-            return par_meas
+        return tf.convert_to_tensor(par_meas, dtype=X_true_pred.dtype)
+        # if tf.is_tensor(X_true_pred):
+        # else:
+        #     return par_meas
 
 
 

py/distributions/tf_loss_func.py
@@ -11,7 +11,8 @@ def reshape_e_to_H(e, ae_input, X, D, cov_sample):
         else:
             E_shape = (tf.shape(D)[1], (tf.shape(D)[0] - tf.shape(cov_sample)[1]))
         E = tf.reshape(e, E_shape)
-        H = tf.matmul(ae_input, E)
+        H = tf.matmul(ae_input, E)  #
+        # H = tf.concat([tf.matmul(ae_input, E), cov_sample], axis=1)  # uncomment if ae_bfgs_cov1
         return H
     else:
         E_shape = (tf.shape(ae_input)[1], tf.shape(D)[0] - tf.shape(cov_sample)[1])
@@ -71,8 +72,6 @@ def tf_gaus_loss_D_single(H, c_i, b_and_D, par_sample, par_meas_i):
 @tf.function
 def tf_gaus_loss_E(e, D, b, x, x_norm, par_sample, par_meas, cov_sample):
     H = reshape_e_to_H(e, x_norm, x, D, cov_sample)
-    # print("H")
-    # print(H.shape)
     y = tf.matmul(H, D) + b
     return tf_gaus_loss(x, y)
 