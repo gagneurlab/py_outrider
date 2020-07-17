fixed fold change calculation, now depends on data_trans class

py/dataset_handling/create_xarray.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 import xarray as xr
 
-import profiles
-import distributions
-import dataset_handling
-
+from profiles import profile_pca, profile_protrider, profile_outrider
+from distributions.dis import dis_gaussian, dis_log_gaussian, dis_neg_bin
+from distributions.loss_dis import loss_dis_gaussian, loss_dis_log_gaussian, loss_dis_neg_bin
+from dataset_handling.input_transform import trans_sf, trans_none, trans_log
+from dataset_handling.preprocess import prepro_sf_log, prepro_none
 
 
 class Create_xarray():
@@ -16,8 +17,8 @@ class Create_xarray():
 
         ### create dict to transform to xarray object
         xrds_dict = {
-            "X":(("sample","meas"), X_file.values),
-            "X_na":(("sample","meas"), np.isfinite(X_file).values )
+            "X": (("sample", "meas"), X_file.values),
+            "X_na": (("sample", "meas"), np.isfinite(X_file).values)
         }
 
         xrds_coords = {
@@ -27,7 +28,7 @@ class Create_xarray():
 
         if args_input["file_sa"] is not None:
             sample_anno = self.get_sample_anno(args_input["file_sa"], X_file)
-            xrds_dict["sample_anno"] = (("sample","sample_anno_col"), sample_anno.values.astype(str))
+            xrds_dict["sample_anno"] = (("sample", "sample_anno_col"), sample_anno.values.astype(str))
             xrds_coords["sample_anno_col"] = sample_anno.columns
 
             if args_input["cov_used"] is not None:
@@ -36,15 +37,15 @@ class Create_xarray():
                 xrds_coords["cov_sample_col"] = cov_sample.columns
 
         if args_input["X_is_outlier"] is not None:
-            X_is_outlier =  self.get_X_is_outlier(args_input["X_is_outlier"], X_file)
-            X_is_outlier[~xrds_dict["X_na"][1]] = np.nan   # force same nan values as in X
+            X_is_outlier = self.get_X_is_outlier(args_input["X_is_outlier"], X_file)
+            X_is_outlier[~xrds_dict["X_na"][1]] = np.nan  # force same nan values as in X
             xrds_dict["X_is_outlier"] = (("sample", "meas"), X_is_outlier.values)
 
-        self.xrds = xr.Dataset(xrds_dict, coords = xrds_coords)
+        self.xrds = xr.Dataset(xrds_dict, coords=xrds_coords)
 
         ### add additional metadata
-        for add_attr in ["encod_dim","num_cpus","output","output_list", "float_type",
-                         "max_iter","verbose","seed","output_plots"] :
+        for add_attr in ["encod_dim", "num_cpus", "output", "output_list", "float_type",
+                         "max_iter", "verbose", "seed", "output_plots"]:
             self.xrds.attrs[add_attr] = args_input[add_attr]
 
         self.xrds["par_sample"] = (("sample"), np.repeat(1, len(self.xrds.coords["sample"])))
@@ -53,12 +54,6 @@ class Create_xarray():
 
         ### preprocess xrds
         self.xrds.attrs["profile"].prepro.prepro_xrds(self.xrds)
-
-
-
-
-
-
 
 
     def read_data_file(self, file_path):
@@ -71,7 +66,7 @@ class Create_xarray():
 
 
     def get_sample_anno(self, sa_file_path, X_file):
-        sample_anno = self.read_data_file(sa_file_path)   # TODO fix with no index col ?
+        sample_anno = self.read_data_file(sa_file_path)  # TODO fix with no index col ?
 
         ### find sample_id column
         sample_col_found = None
@@ -84,7 +79,7 @@ class Create_xarray():
         elif len(sample_anno[sample_col_found]) != len(set(sample_anno[sample_col_found])):
             raise ValueError(f"duplicates found in file_sa sample_id column: {sample_col_found}")
         else:
-            sample_anno.rename(columns = {sample_col_found:"sample_id"}, inplace = True)
+            sample_anno.rename(columns={sample_col_found: "sample_id"}, inplace=True)
             sample_anno.set_index(sample_anno["sample_id"], inplace=True)
 
         ### sort according to X_file and remove unnecessary
@@ -107,14 +102,14 @@ class Create_xarray():
             if len(col.cat.categories) == 1:
                 cov_sample.drop(c, axis=1, inplace=True, errors="ignore")
             elif len(col.cat.categories) == 2:
-                only_01 = [True if x in [0,1] else False for x in col.cat.categories]
+                only_01 = [True if x in [0, 1] else False for x in col.cat.categories]
                 if all(only_01) is True:
                     # print(f"only_01: {c}")
                     pass
                 else:
                     # print(f"2 cat: {c}")
                     oneh = pd.get_dummies(cov_sample[c])
-                    cov_sample[c] = oneh.iloc[:,0]
+                    cov_sample[c] = oneh.iloc[:, 0]
             else:
                 # print(f">2 cat: {c}")
                 oneh = pd.get_dummies(cov_sample[c])
@@ -146,12 +141,12 @@ class Create_xarray():
 
 
     def get_profile(self, profile):
-        if profile["profile"].lower()=="outrider":
-            prof = profiles.profile_outrider.Profile_outrider()
-        elif profile["profile"].lower()=="protrider":
-            prof = profiles.profile_protrider.Profile_protrider()
-        elif profile["profile"].lower()=="pca":
-            prof = profiles.profile_pca.Profile_pca()
+        if profile["profile"].lower() == "outrider":
+            prof = profile_outrider.Profile_outrider()
+        elif profile["profile"].lower() == "protrider":
+            prof = profile_protrider.Profile_protrider()
+        elif profile["profile"].lower() == "pca":
+            prof = profile_pca.Profile_pca()
 
         ### edit profile if specified in input
         if profile["prepro"] is not None:
@@ -168,51 +163,39 @@ class Create_xarray():
         return prof
 
 
-
     def get_profile_distribution(self, prof_dis):
         if prof_dis.lower() == "neg_bin":
-            return distributions.dis.dis_neg_bin.Dis_neg_bin
+            return dis_neg_bin.Dis_neg_bin
         elif prof_dis.lower() == "gaus":
-            return distributions.dis.dis_gaussian.Dis_gaussian
+            return dis_gaussian.Dis_gaussian
         else:
             print("dis not found")
 
+
     def get_profile_data_trans(self, prof_dt):
         if prof_dt.lower() == "sf":
-            return dataset_handling.input_transform.trans_sf.Trans_sf
+            return trans_sf.Trans_sf
         elif prof_dt.lower() == "log":
-            return dataset_handling.input_transform.trans_log.Trans_log
+            return trans_log.Trans_log
         elif prof_dt.lower() == "none":
-            return dataset_handling.input_transform.trans_none.Trans_none
+            return trans_none.Trans_none
         else:
             print("data_trans not found")
 
+
     def get_profile_loss_dis(self, prof_loss):
         if prof_loss.lower() == "neg_bin":
-            return distributions.loss_dis.loss_dis_neg_bin.Loss_dis_neg_bin
+            return loss_dis_neg_bin.Loss_dis_neg_bin
         elif prof_loss.lower() == "gaus":
-            return distributions.loss_dis.loss_gaussian.Loss_gaussian
+            return loss_dis_gaussian.Loss_dis_gaussian
         else:
             print("loss_dis not found")
 
+
     def get_profile_prepro(self, prof_pre):
         if prof_pre.lower() == "sf_log":
-            return dataset_handling.preprocess.prepro_sf_log.Prepro_sf_log
+            return prepro_sf_log.Prepro_sf_log
         elif prof_pre.lower() == "none":
-            return dataset_handling.preprocess.prepro_none.Prepro_none
+            return prepro_none.Prepro_none
         else:
             print("prepro not found")
-
-
-
-
-
-
-
-
-
-
-
-
-
-

py/dataset_handling/input_transform/trans_log.py
@@ -7,7 +7,7 @@ from distributions.dis.dis_abstract import Dis_abstract
 from dataset_handling.input_transform.trans_abstract import Trans_abstract
 from utilis.stats_func import multiple_testing_nan
 from distributions.loss_dis.loss_dis_gaussian import Loss_dis_gaussian
-
+from utilis.stats_func import get_fc
 
 
 
@@ -34,10 +34,10 @@ class Trans_log(Trans_abstract):
         #     return np.power(y, 2)
 
 
+
     @staticmethod
-    def get_logfc(X, X_pred):
-        return np.log1p(xrds["X"])
-        # return np.log(xrds["X"] + 1)
+    def get_logfc(X_trans, X_trans_pred, **kwargs):
+        return get_fc(X_trans, X_trans_pred)
 
 
 

py/dataset_handling/input_transform/trans_sf.py
@@ -7,7 +7,7 @@ from distributions.dis.dis_abstract import Dis_abstract
 from dataset_handling.input_transform.trans_abstract import Trans_abstract
 from utilis.stats_func import multiple_testing_nan
 from distributions.loss_dis.loss_dis_gaussian import Loss_dis_gaussian
-
+from utilis.stats_func import get_logfc
 
 
 
@@ -44,6 +44,15 @@ class Trans_sf(Trans_abstract):
             return tfm.exp(y) * tf.expand_dims(sf, 1)
         else:
             return np.exp(y) * np.expand_dims(sf,1)
+
+
+
+    @staticmethod
+    def get_logfc(X_trans, X_trans_pred, par_sample, **kwargs):
+        X = Trans_sf.rev_transform(X_trans, par_sample=par_sample)
+        X_pred = Trans_sf.rev_transform(X_trans_pred, par_sample=par_sample)
+        return get_logfc(X, X_pred)
+
 
 
 