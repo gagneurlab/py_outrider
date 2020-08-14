4341325340d5f32e4904e472ed582dd6de8c643e
Stefan Loipfinger <loipfins@in.tum.de>
2020-08-14 19:24:08 +0200

    added some more comments and removed hard coded pathes

--- py/main/full_run.py
+++ py/main/full_run.py
@@ -6,7 +6,7 @@ from dataset_handling.model_dataset import Model_dataset
 from dataset_handling.custom_output import Custom_output
 from utilis.xarray_output import xrds_to_zarr
 from hyperpar_opt import Hyperpar_opt
-
+from utilis.print_func import print_time
 
 
 
@@ -14,51 +14,44 @@ from hyperpar_opt import Hyperpar_opt
 class Full_run():
 
     def __init__(self, args_input):
+        print_time('parser check for correct input arguments')
         args_mod = check_parser.Check_parser(args_input).args_mod
-        print('parser check')
+
+        print_time('create xarray object out of input data')
         xrds = Create_xarray(args_mod).xrds
-        print('xarray created')
         print(xrds)
 
 
+        print_time('create model dataset class')
         model_ds = Model_dataset(xrds)
-        print('dataset created')
-        model_ds.inject_outlier(inj_freq=1e-3, inj_mean=3, inj_sd=1.6)
-        # print('outlier injected')
 
 
         if xrds.attrs["encod_dim"] is None:
-            print('encod_dim is None -> running hyperpar-opt')
+            print_time('encod_dim is None -> run hyperparameter optimisation')
             Hyperpar_opt(model_ds)
 
-        print('noise injected')
+        print_time('inject noise if specified')
         model_ds.inject_noise(inj_freq=1, inj_mean=0, inj_sd=1)
 
 
         fit_model = model_ds.profile.fit_model(model_ds)
-        print('run model')
+        print_time('start running model fit')
         xrds = fit_model.run_model_fit()
 
+        print_time('save model dataset to file')
         xrds_to_zarr(xrds_obj=xrds, output_path=xrds.attrs["output"])
 
 
         if "X_is_outlier" in xrds:
             pre_rec = st.get_prec_recall(xrds["X_pvalue"].values, xrds["X_is_outlier"].values)
-            print(f'precision-recall: { pre_rec["auc"] }')
-
-        ### export
+            print_time(f'precision-recall: { pre_rec["auc"] }')
 
 
-        print('start plotting')
+        print_time('start creating custom output if specified')
         Custom_output(xrds)
 
 
-
-
-
-        print('finished')
-
-
+        print_time('finished whole run')
 
 
 
--- py/main/main.py
+++ py/main/main.py
@@ -13,57 +13,11 @@ from full_run import Full_run
 
 
 def main():
-    # print(sys.argv)
     args = input_parser.parse_args(sys.argv[1:])
 
-    print(args)
-    if args['file_meas'] is not None:    # if new tables are given, otherwise execute example
-        Full_run(args_input=args)
-        # print_func.print_dict(args)
-        pass
+    print_func.print_dict(args)
+    Full_run(args_input=args)
 
-    else:
-        # folder_path = '/home/stefan/gagneurlab/home/Documents/mofa_outrider/01_example_blood_subset/'
-        # folder_path = '/home/stefan/gagneurlab/home/Documents/mofa_outrider/06_sample_blood_outlier_z3/'
-        folder_path = '/home/stefan/Desktop/py_outrider/gene_subset_01/'
-        folder_path = '/home/stefan/Desktop/py_outrider/protein_neutropenia/'
-
-
-        # args2 = { "file_meas" : folder_path+"counts_raw.csv", "encod_dim": 3, 'verbose':True, 'num_cpus':5,
-        args2 = { "file_meas" : folder_path+"counts_raw.csv", "encod_dim": None, 'verbose':True, 'num_cpus':5, 'seed':5,
-                  # 'X_is_outlier': folder_path+"trueCorruptions.csv", "max_iter": 2, "profile": "outrider"
-                  # 'X_is_outlier': folder_path+"trueCorruptions.csv", "max_iter": 1, "profile": "pca"
-                  # 'X_is_outlier': folder_path+"trueCorruptions.csv", "max_iter": 2, "profile": "protrider"
-                  "max_iter": 1, "profile": "protrider"
-                # ,'file_sa': folder_path+ 'sa_file_artificially.csv', 'covariates': ["batch", "oneh"]
-                ,'file_sa': folder_path+ 'san_notechrep.csv', 'covariates': ["sex", "date_processed_y_m_d"]
-                  }
-
-        # folder_path = '/home/stefan/gagneurlab/s/project/protrider/loipf/data/prok_version_P20200317_with_outliers/'
-        # args2 = { "file_meas" : folder_path+"X_raw_out.csv", "encod_dim": 25, 'verbose':True, 'num_cpus':5,
-        #           # 'X_is_outlier': folder_path+"trueCorruptions.csv", "max_iter": 2, "profile": "outrider"
-        #           # 'X_is_outlier': folder_path+"trueCorruptions.csv", "max_iter": 1, "profile": "pca"
-        #           'X_is_outlier': folder_path+"X_out_pos.csv", "max_iter": 10, "profile": "protrider"
-        #         ,'file_sa': folder_path+ 'prok_batches.csv', 'covariates': ["PROTEOMICS_BATCH","gender","INSTRUMENT"]
-        #           }
-
-
-        folder_path = '/home/stefan/gagneurlab/s/project/protrider/loipf/results/20200713_min_sample_size/datasets/'
-        args2 = { "file_meas" : folder_path+"tmt_0_1.csv", "encod_dim": 5, 'verbose':True, 'num_cpus':5,
-                  "max_iter": 20, "profile": "protrider" #"protrider_cov1" #"protrider"
-                ,'file_sa': '/home/stefan/gagneurlab/s/project/protrider/loipf/data/prok_version_P20200317_paper_samples/py_outrider/prok_batches.csv',
-                  'covariates': ["PROTEOMICS_BATCH","gender","INSTRUMENT"], "seed":5,
-                  'X_is_outlier': '/home/stefan/Desktop/X_is_outlier.csv', "float_type":"float32",
-                  "output":'/home/stefan/gagneurlab/s/project/protrider/loipf/results/20200713_min_sample_size/trained_obj/protrider/tmt_0_1/xrds_output_pytest2/'
-                  }
-
-
-
-
-        args.update(args2)
-        Full_run(args_input=args)
-
-        # print_func.print_dict(args)
 
 
 
