d1fd95cc107befe95e762142630d90e0394b5ac1
Ines Scheller <scheller@in.tum.de>
2021-02-23 11:53:41 +0100

    add example run .ipynb

--- py_outrider/__init__.py
+++ py_outrider/__init__.py
@@ -1,3 +1,5 @@
+"""Python backend package for OUTRIDER2 R/Bioconductor package."""
+
 # import os
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
---
+++ py_outrider/utils/__init__.py
@@ -0,0 +1,1 @@
+"""Contains py_outrider.utils functions."""
